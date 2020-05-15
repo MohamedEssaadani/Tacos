fb9eeb1704f739a6916ce0a02408c641604e0a58
Essaadani <essaadani80@gmail.com>
2020-05-15 01:37:28 +0000

    sign in & sign up for clients

--- resources/views/partials/client-side/header.blade.php
+++ resources/views/partials/client-side/header.blade.php
@@ -2,12 +2,36 @@
     <div class="container">
         <a class="logo" href="#"><img src="images/tacos-logo.png" alt="Logo"></a>
 
+        @guest
         <div class="right-area">
-            <h6 style="float:left;"><a class="plr-20 color-white btn-fill-primary" href="#">Login</a></h6>
+            <h6 style="float:left;"><a class="plr-20 color-white btn-fill-primary" href="{{route('login')}}">Login</a>
+            </h6>
             <h6 style="float:right;"><a class="plr-20 color-white btn-fill-primary" style="margin-left:10px;"
-                    href="#">Sign Up</a></h6>
+                    href="{{route('register')}}">Sign Up</a></h6>
         </div>
         <!-- right-area -->
+        @else
+        <div class="right-area">
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        </div>
+        @endguest
+
 
         <a class="menu-nav-icon" data-menu="#main-menu" href="#"><i class="ion-navicon"></i></a>
 
